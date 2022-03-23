838a059aef7e65fd90b54a6862d687b45c62988b
otrachea <francesca.ho459@gmail.com>
2022-03-23 16:35:30 +0000

    Can't register with empty email or password

--- express_server.js
+++ express_server.js
@@ -77,6 +77,10 @@ app.get("/register", (req, res) => {
 })
 
 app.post("/register", (req, res) => {
+  if ([req.body.email, req.body.password].includes("")) {
+    res.statusCode = 400;
+    return res.send("Error 400: Cannot have empty email or password");
+  }
   let userID = generateRandomString();
   users[userID] = { id: userID, email: req.body.email, password: req.body.password };
   res.cookie("userID", userID).redirect("/urls");

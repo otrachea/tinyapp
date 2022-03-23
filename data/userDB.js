3385347da727db0e5a0d1807a11704a0587af7bc
otrachea <francesca.ho459@gmail.com>
2022-03-23 22:25:41 +0000

    Moved users and urlDatabases to separate files in data folder

---
+++ data/userDB.js
@@ -0,0 +1,14 @@
+const users = {
+  'aJ48lW': {
+    userID: "aJ48lW",
+    email: "fdsa@gmail.com",
+    password: "test"
+  },
+  'zusVIt': {
+    userID: 'zusVIt',
+    email: "test@test.com",
+    password: "a"
+  }
+};
+
+module.exports = { users };

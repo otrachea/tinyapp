037e243d5fea4795e098699525b50019f97f6a52
otrachea <francesca.ho459@gmail.com>
2022-03-23 17:20:43 +0000

    Moved helper functions here

---
+++ helpers.js
@@ -0,0 +1,22 @@
+const generateRandomString = () => {
+  let alpha = "abcdefghijklmnopqrsztuvwyz";
+  alpha += alpha.toUpperCase() + "0123456789";
+  let result = "";
+
+  for (let i = 0; i < 6; i++) {
+    result += alpha[Math.floor(Math.random() * 52)];
+  }
+
+  return result;
+}
+
+const emailLookup = (users, email) => {
+  for (const user of Object.values(users)) {
+    if (user.email === email) {
+      return true;
+    }
+  }
+  return false;
+}
+
+module.exports = { generateRandomString, emailLookup };

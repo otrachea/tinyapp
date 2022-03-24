7d0be65f7e859457c0bc41259a4d8788bca51297
otrachea <francesca.ho459@gmail.com>
2022-03-24 20:27:43 +0000

    Commented out test users and test urls

--- data/urlDB.js
+++ data/urlDB.js
@@ -1,12 +1,12 @@
 const urlDatabase = {
-  "b2xVn2": {
-    longURL: "http://www.lighthouselabs.ca",
-    userID: "aJ48lW"
-  },
-  "9sm5xK": {
-    longURL: "http://www.google.com",
-    userID: "aJ48lW"
-  }
+  // "b2xVn2": {
+  //   longURL: "http://www.lighthouselabs.ca",
+  //   userID: "aJ48lW"
+  // },
+  // "9sm5xK": {
+  //   longURL: "http://www.google.com",
+  //   userID: "aJ48lW"
+  // }
 };
 
 module.exports = { urlDatabase };
